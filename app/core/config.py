665d020d8cf2e4147743c212881bb5326677b294
omii9079 <landeomkar9079@gmail.com>
2026-03-05 17:30:55 +0530

    deployed cors origin for render

--- app/core/config.py
+++ app/core/config.py
@@ -22,6 +22,7 @@ class Settings(BaseSettings):
 
     """CORS settings"""
     CORS_ORIGINS: List[str] = [
+        "https://sourcio-front-end.onrender.com",
         "http://localhost:3000",
         "http://127.0.0.1:3000",
         "http://localhost:3001",
